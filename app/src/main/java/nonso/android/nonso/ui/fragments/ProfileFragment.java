9f8a506c53bb4c9c436739d31b10c7ccbaecf232
namdizy <nnamdianimate@gmail.com>
2018-04-03 08:29:07 -0700

    Image selection updates

--- app/src/main/java/nonso/android/nonso/ui/fragments/ProfileFragment.java
+++ app/src/main/java/nonso/android/nonso/ui/fragments/ProfileFragment.java
@@ -3,14 +3,10 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,23 +17,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-import com.theartofdev.edmodo.cropper.CropImage;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -52,6 +35,7 @@ public class ProfileFragment extends Fragment {
     @BindView(R.id.tv_profile_username) TextView mUsernameText;
     @BindView(R.id.fab_profile_create) FloatingActionButton mCreateFab;
     @BindView(R.id.profile_image) ImageView mUserProfileImage;
+
 
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
@@ -118,21 +102,6 @@ public class ProfileFragment extends Fragment {
         return view;
     }
 
-
-    public BottomSheetBehavior.BottomSheetCallback bottomSheetCallback(){
-        return new BottomSheetBehavior.BottomSheetCallback() {
-            @Override
-            public void onStateChanged(@NonNull View bottomSheet, int newState) {
-                // React to state change
-            }
-
-            @Override
-            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
-                // React to dragging events
-            }
-        };
-    }
-
     @OnClick(R.id.btn_profile_settings)
     public void onSettingsClick(View view){
 
@@ -151,103 +120,60 @@ public class ProfileFragment extends Fragment {
 
     @OnClick(R.id.profile_image)
     public void profileImageOnclick(View view){
-        CropImage.startPickImageActivity(getContext(), this);
+        startPickImageActivity();
     }
+
+    public void startPickImageActivity(){
+        MaterialDialog dialog = new MaterialDialog.Builder(getContext())
+                .title(R.string.profile_select_image)
+                .customView(R.layout.select_image_source_dialog, true)
+                .show();
+
+        View view = dialog.getCustomView();
+    }
+
+
+    @OnClick(R.id.btn_from_gallery)
+    public void onlick(View v){
+        Toast.makeText(getContext(), "Cancelling,from gallery", Toast.LENGTH_LONG).show();
+    }
+
 
     @Override
     @SuppressLint("NewApi")
     public void onActivityResult(int requestCode, int resultCode, Intent data){
-        if(requestCode == CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE && resultCode == Activity.RESULT_OK){
-
-            Uri imageUri = CropImage.getPickImageResultUri(getContext(), data);
-
-            if(CropImage.isReadExternalStoragePermissionsRequired(getContext(), imageUri)){
-                mCropImageUri = imageUri;
-                requestPermissions(new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, CropImage.PICK_IMAGE_PERMISSIONS_REQUEST_CODE );
-            }else{
-                Log.v("Profile Fragment pick", "imageurl"+ imageUri);
-                startCropImageActivity(mCropImageUri);
-            }
-
-        }
-        else if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK ){
-
-            CropImage.ActivityResult result = CropImage.getActivityResult(data);
-            if (resultCode == Activity.RESULT_OK) {
-                Uri resultUri = result.getUri();
-                mUserProfileImage.setImageURI(resultUri);
-
-                String userPicture = mUser.getDisplayName()+".jpg";
-
-                // Create a storage reference from our app
-                StorageReference storageRef = mStorage.getReference();
-
-                // Create a reference to profile image
-                StorageReference mountainsRef = storageRef.child(userPicture);
-
-                // Create a reference to 'images/profile image.jpg'
-                StorageReference mountainImagesRef = storageRef.child("images/"+userPicture);
-
-                // While the file names are the same, the references point to different files
-                mountainsRef.getName().equals(mountainImagesRef.getName());    // true
-                mountainsRef.getPath().equals(mountainImagesRef.getPath());
-                try{
-                    InputStream stream = new FileInputStream(new File(resultUri.toString()));
-
-                    UploadTask uploadTask = mountainsRef.putStream(stream);
-                    uploadTask.addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception exception) {
-                            // Handle unsuccessful uploads
-                        }
-                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
-                            Uri downloadUrl = taskSnapshot.getDownloadUrl();
-                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                                    .setPhotoUri(Uri.parse(downloadUrl.toString()))
-                                    .build();
-
-                            mUser.updateProfile(profileUpdates)
-                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            if (task.isSuccessful()) {
-                                                Log.d("FRAGMENT PROFILE", "User profile updated.");
-                                            }
-                                        }
-                                    });
-                        }
-                    });
-
-                }catch (FileNotFoundException e){
-
-                }
-
-
-
-
-            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-                Exception error = result.getError();
-            }
-        }
+//        if(requestCode == CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE && resultCode == Activity.RESULT_OK){
+//
+//            Uri imageUri = CropImage.getPickImageResultUri(getContext(), data);
+//
+//            if(CropImage.isReadExternalStoragePermissionsRequired(getContext(), imageUri)){
+//                mCropImageUri = imageUri;
+//                requestPermissions(new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, CropImage.PICK_IMAGE_PERMISSIONS_REQUEST_CODE );
+//            }else{
+//                Log.v("Profile Fragment pick", "imageurl"+ imageUri);
+//                startCropImageActivity(mCropImageUri);
+//            }
+//
+//        }
+//        else if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK ){
+//
+//        }
     }
 
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        if (requestCode == CropImage.PICK_IMAGE_PERMISSIONS_REQUEST_CODE) {
-            if (mCropImageUri != null && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // required permissions granted, start crop image activity
-                startCropImageActivity(mCropImageUri);
-            } else {
-                Toast.makeText(getContext(), "Cancelling, required permissions are not granted", Toast.LENGTH_LONG).show();
-            }
-        }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[],@NonNull int[] grantResults) {
+//        if (requestCode == CropImage.PICK_IMAGE_PERMISSIONS_REQUEST_CODE) {
+//            if (mCropImageUri != null && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                // required permissions granted, start crop image activity
+//                startCropImageActivity(mCropImageUri);
+//            } else {
+//                Toast.makeText(getContext(), "Cancelling, required permissions are not granted", Toast.LENGTH_LONG).show();
+//            }
+//        }
     }
 
 
     private void startCropImageActivity(Uri imageUri) {
-        CropImage.activity(imageUri)
-                .start(getContext(), this);
+//        CropImage.activity(imageUri)
+//                .start(getContext(), this);
     }
 }
